41f06b736cec2d0941af10278d26d7f840e25c98
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2022-11-10 13:09:05 +0700

    initial setup

---
+++ client/src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className="flex justify-center p-4">
+      <h1 className="text-4xl font-bold">Shuffle Game</h1>
+    </main>
+  );
+};
+
+export default App;

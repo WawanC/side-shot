e0d3cc1bd74469d6ac3bf224232be059bdafb22f
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2022-11-12 21:13:10 +0700

    socket.io setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,13 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 
-app.listen(8000, () => {
-  console.log("Listening on port 8000...");
+io.on("connection", (socket) => {
+  console.log(`New User Connected : ${socket.id}`);
 });
+
+httpServer.listen(8000);

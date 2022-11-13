22aab7a6a255a07b305f24ed05d5ddc775611beb
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2022-11-13 12:04:49 +0700

    lobby server socket connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,28 @@
 import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
+import Player from "./interfaces/player";
+
+let players: Player[] = [];
 
 const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
-  console.log(`New User Connected : ${socket.id}`);
+  socket.emit("players", players);
+
+  socket.on("join", (username: string) => {
+    const newPlayer: Player = { id: socket.id, username: username };
+    players.push(newPlayer);
+
+    io.emit("players", players);
+  });
+
+  socket.on("disconnect", (reason) => {
+    players = players.filter((player) => player.id !== socket.id);
+    io.emit("players", players);
+  });
 });
 
 httpServer.listen(8000);
